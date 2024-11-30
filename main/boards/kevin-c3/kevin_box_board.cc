59b71eabd3b7631deba81db9e25f0422a5efcfe9
Kevincoooool <33611679+Kevincoooool@users.noreply.github.com>
2024-11-30 22:18:19 +0800

    test fine

--- main/boards/kevin-c3/kevin_box_board.cc
+++ main/boards/kevin-c3/kevin_box_board.cc
@@ -1,5 +1,6 @@
 #include "wifi_board.h"
 #include "audio_codecs/es8311_audio_codec.h"
+#include "display/st7789_display.h"
 #include "application.h"
 #include "button.h"
 #include "led.h"
@@ -8,6 +9,9 @@
 #include <wifi_station.h>
 #include <esp_log.h>
 #include <driver/i2c_master.h>
+#include <driver/spi_common.h>
+#include <esp_lcd_panel_vendor.h>
+#include "esp_efuse_table.h"
 
 #define TAG "KevinBoxBoard"
 
@@ -15,6 +19,7 @@ class KevinBoxBoard : public WifiBoard {
 private:
     i2c_master_bus_handle_t codec_i2c_bus_;
     Button boot_button_;
+    St7789Display* display_;
 
     void InitializeCodecI2c() {
         // Initialize I2C peripheral
@@ -32,7 +37,16 @@ private:
         };
         ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
     }
-
+    void InitializeSpi() {
+        spi_bus_config_t buscfg = {};
+        buscfg.mosi_io_num = GPIO_NUM_5;
+        buscfg.miso_io_num = GPIO_NUM_NC;
+        buscfg.sclk_io_num = GPIO_NUM_3;
+        buscfg.quadwp_io_num = GPIO_NUM_NC;
+        buscfg.quadhd_io_num = GPIO_NUM_NC;
+        buscfg.max_transfer_sz = DISPLAY_WIDTH * DISPLAY_HEIGHT * sizeof(uint16_t);
+        ESP_ERROR_CHECK(spi_bus_initialize(SPI2_HOST, &buscfg, SPI_DMA_CH_AUTO));
+    }
     void InitializeButtons() {
         boot_button_.OnClick([this]() {
             auto& app = Application::GetInstance();
@@ -47,7 +61,38 @@ private:
             Application::GetInstance().StopListening();
         });
     }
+    void InitializeSt7789Display() {
+        esp_lcd_panel_io_handle_t panel_io = nullptr;
+        esp_lcd_panel_handle_t panel = nullptr;
+        // 液晶屏控制IO初始化
+        ESP_LOGD(TAG, "Install panel IO");
+        esp_lcd_panel_io_spi_config_t io_config = {};
+        io_config.cs_gpio_num = GPIO_NUM_4;
+        io_config.dc_gpio_num = GPIO_NUM_6;
+        io_config.spi_mode = 3;
+        io_config.pclk_hz = 80 * 1000 * 1000;
+        io_config.trans_queue_depth = 10;
+        io_config.lcd_cmd_bits = 8;
+        io_config.lcd_param_bits = 8;
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi(SPI2_HOST, &io_config, &panel_io));
 
+        // 初始化液晶屏驱动芯片ST7789
+        ESP_LOGD(TAG, "Install LCD driver");
+        esp_lcd_panel_dev_config_t panel_config = {};
+        panel_config.reset_gpio_num = GPIO_NUM_NC;
+        panel_config.rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB;
+        panel_config.bits_per_pixel = 16;
+        ESP_ERROR_CHECK(esp_lcd_new_panel_st7789(panel_io, &panel_config, &panel));
+        
+        esp_lcd_panel_reset(panel);
+
+        esp_lcd_panel_init(panel);
+        esp_lcd_panel_invert_color(panel, true);
+        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
+        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+        display_ = new St7789Display(panel_io, panel, DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT,
+                                    DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY);
+    }
 public:
     KevinBoxBoard() :
         boot_button_(BOOT_BUTTON_GPIO) {
@@ -55,8 +100,10 @@ public:
 
     virtual void Initialize() override {
         ESP_LOGI(TAG, "Initializing KevinBoxBoard");
-
+        esp_efuse_write_field_bit(ESP_EFUSE_VDD_SPI_AS_GPIO);
         InitializeCodecI2c();
+        InitializeSpi();
+        InitializeSt7789Display();
         InitializeButtons();
 
         WifiBoard::Initialize();
@@ -72,6 +119,9 @@ public:
             AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
             AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
         return &audio_codec;
+    }
+    virtual Display* GetDisplay() override {
+        return display_;
     }
 };
 
