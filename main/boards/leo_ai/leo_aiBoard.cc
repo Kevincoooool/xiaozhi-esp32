337235f2ad37e2a2a66f02d679bf6238fe655a1e
Kevincoooool <33611679+Kevincoooool@users.noreply.github.com>
2025-01-12 18:24:59 +0800

    1-12

--- main/boards/leo_ai/leo_aiBoard.cc
+++ main/boards/leo_ai/leo_aiBoard.cc
@@ -3,7 +3,12 @@
 #include "audio_codecs/box_audio_codec.h"
 #include "application.h"
 #include "button.h"
-#include "led.h"
+
+
+
+#include "iot/thing_manager.h"
+#include "led/single_led.h"
+
 #include "config.h"
 #include "i2c_device.h"
 #include <driver/i2c_master.h>
@@ -48,55 +53,66 @@ private:
     {
         // boot_button_.OnClick([this]()
         //                      { Application::GetInstance().ToggleChatState(); });
+
         boot_button_.OnPressDown([this]() {
             Application::GetInstance().StartListening();
         });
         boot_button_.OnPressUp([this]() {
             Application::GetInstance().StopListening();
         });
-        volume_up_button_.OnClick([this]()
-                                  {
+
+        volume_up_button_.OnClick([this]() {
             auto codec = GetAudioCodec();
             auto volume = codec->output_volume() + 10;
             if (volume > 100) {
                 volume = 100;
             }
-            codec->SetOutputVolume(volume); });
+            codec->SetOutputVolume(volume);
+            GetDisplay()->ShowNotification("音量 " + std::to_string(volume));
+        });
 
-        volume_up_button_.OnLongPress([this]()
-                                      {
-            auto codec = GetAudioCodec();
-            codec->SetOutputVolume(100); });
+        volume_up_button_.OnLongPress([this]() {
+            GetAudioCodec()->SetOutputVolume(100);
+            GetDisplay()->ShowNotification("最大音量");
+        });
 
-        volume_down_button_.OnClick([this]()
-                                    {
+        volume_down_button_.OnClick([this]() {
             auto codec = GetAudioCodec();
             auto volume = codec->output_volume() - 10;
             if (volume < 0) {
                 volume = 0;
             }
-            codec->SetOutputVolume(volume); });
+            codec->SetOutputVolume(volume);
+            GetDisplay()->ShowNotification("音量 " + std::to_string(volume));
+        });
 
-        volume_down_button_.OnLongPress([this]()
-                                        {
-            auto codec = GetAudioCodec();
-            codec->SetOutputVolume(0); });
+        volume_down_button_.OnLongPress([this]() {
+            GetAudioCodec()->SetOutputVolume(0);
+            GetDisplay()->ShowNotification("已静音");
+        });
     }
-public:
 
-    Leo_AIBoard() : 
+        // 物联网初始化，添加对 AI 可见设备
+    void InitializeIot() {
+        auto& thing_manager = iot::ThingManager::GetInstance();
+        thing_manager.AddThing(iot::CreateThing("Speaker"));
+    }
+
+public:
+    Leo_AIBoard() :
         boot_button_(BOOT_BUTTON_GPIO),
         volume_up_button_(VOLUME_UP_BUTTON_GPIO),
         volume_down_button_(VOLUME_DOWN_BUTTON_GPIO) {
-    }
-    
-    virtual void Initialize() override {
+
+       
+
         ESP_LOGI(TAG, "Initializing Leo_AIBoard");
         InitializeI2c();
         axp2101_ = new Axp2101(codec_i2c_bus_, AXP2101_I2C_ADDR);
 
-        InitializeButtons();
-        WifiBoard::Initialize();
+        InitializeButtons(); 
+        InitializeIot();
+        // WifiBoard::Initialize();
     }
         virtual AudioCodec* GetAudioCodec() override {
         static BoxAudioCodec* audio_codec = nullptr;
@@ -108,10 +124,10 @@ public:
         }
         return audio_codec;
     }
-    virtual Led* GetBuiltinLed() override {
-        static Led led(BUILTIN_LED_GPIO);
-        return &led;
-    }    
+    // virtual Led* GetBuiltinLed() override {
+    //     static Led led(BUILTIN_LED_GPIO);
+    //     return &led;
+    // }    
         virtual Display* GetDisplay() override {
         static NoDisplay display;
         return &display;
