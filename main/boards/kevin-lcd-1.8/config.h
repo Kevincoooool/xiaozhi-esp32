45af88cef897d7272f9a0805b2eded33d83657de
Kevincoooool <33611679+Kevincoooool@users.noreply.github.com>
2025-04-27 18:44:47 +0800

    显示正常

---
+++ main/boards/kevin-lcd-1.8/config.h
@@ -0,0 +1,68 @@
+
+#ifndef _BOARD_CONFIG_H_
+#define _BOARD_CONFIG_H_
+
+#include <driver/gpio.h>
+
+#define AUDIO_INPUT_SAMPLE_RATE  16000
+#define AUDIO_OUTPUT_SAMPLE_RATE 16000
+
+#define AUDIO_I2S_GPIO_MCLK GPIO_NUM_4
+#define AUDIO_I2S_GPIO_WS GPIO_NUM_7
+#define AUDIO_I2S_GPIO_BCLK GPIO_NUM_5
+#define AUDIO_I2S_GPIO_DIN  GPIO_NUM_6
+#define AUDIO_I2S_GPIO_DOUT GPIO_NUM_15
+
+#define AUDIO_CODEC_PA_PIN       GPIO_NUM_18
+#define AUDIO_CODEC_I2C_SDA_PIN  GPIO_NUM_17
+#define AUDIO_CODEC_I2C_SCL_PIN  GPIO_NUM_16
+#define AUDIO_CODEC_ES8311_ADDR  ES8311_CODEC_DEFAULT_ADDR
+
+#define BUILTIN_LED_GPIO        GPIO_NUM_NC
+#define BOOT_BUTTON_GPIO        GPIO_NUM_0
+#define VOLUME_UP_BUTTON_GPIO   GPIO_NUM_NC
+#define VOLUME_DOWN_BUTTON_GPIO GPIO_NUM_NC
+
+#define RESET_NVS_BUTTON_GPIO       GPIO_NUM_NC
+#define RESET_FACTORY_BUTTON_GPIO   GPIO_NUM_NC
+
+#define DISPLAY_SDA_PIN GPIO_NUM_NC
+#define DISPLAY_SCL_PIN GPIO_NUM_NC
+#define DISPLAY_WIDTH   360
+#define DISPLAY_HEIGHT  360
+#define DISPLAY_SWAP_XY  false
+#define DISPLAY_MIRROR_X false
+#define DISPLAY_MIRROR_Y false
+#define BACKLIGHT_INVERT false
+#define QSPI_LCD_H_RES           (360)
+#define QSPI_LCD_V_RES           (360)
+#define QSPI_LCD_BIT_PER_PIXEL   (16)
+
+#define QSPI_LCD_HOST           SPI2_HOST
+#define QSPI_PIN_NUM_LCD_PCLK   GPIO_NUM_48
+#define QSPI_PIN_NUM_LCD_CS     GPIO_NUM_39
+#define QSPI_PIN_NUM_LCD_DATA0  GPIO_NUM_41
+#define QSPI_PIN_NUM_LCD_DATA1  GPIO_NUM_38
+#define QSPI_PIN_NUM_LCD_DATA2  GPIO_NUM_47
+#define QSPI_PIN_NUM_LCD_DATA3  GPIO_NUM_21
+#define QSPI_PIN_NUM_LCD_RST    GPIO_NUM_8
+#define QSPI_PIN_NUM_LCD_BL     GPIO_NUM_40
+#define KEVIN_ST77916_PANEL_BUS_QSPI_CONFIG(sclk, d0, d1, d2, d3, max_trans_sz) \
+    {                                                                             \
+        .data0_io_num = d0,                                                       \
+        .data1_io_num = d1,                                                       \
+        .sclk_io_num = sclk,                                                      \
+        .data2_io_num = d2,                                                       \
+        .data3_io_num = d3,                                                       \
+        .max_transfer_sz = max_trans_sz,                                          \
+    }
+#define DISPLAY_OFFSET_X  0
+#define DISPLAY_OFFSET_Y  0
+
+#define DISPLAY_BACKLIGHT_PIN GPIO_NUM_40
+#define DISPLAY_BACKLIGHT_OUTPUT_INVERT false
+
+#define ML307_RX_PIN GPIO_NUM_12
+#define ML307_TX_PIN GPIO_NUM_13
+
+#endif // _BOARD_CONFIG_H_
---
+++ main/boards/kevin-lcd-1.8/kevin-lcd_1_8_board.cc
@@ -0,0 +1,169 @@
+#include "wifi_board.h"
+#include "ml307_board.h"
+#include "audio_codecs/es8311_audio_codec.h"
+#include "display/lcd_display.h"
+#include "system_reset.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "iot/thing_manager.h"
+#include "led/single_led.h"
+
+#include <esp_log.h>
+#include <esp_lcd_panel_vendor.h>
+#include <driver/i2c_master.h>
+#include <wifi_station.h>
+#include <esp_lcd_st77916.h>
+
+#define TAG "kevin-lcd1.8"
+
+LV_FONT_DECLARE(font_puhui_20_4);
+LV_FONT_DECLARE(font_awesome_20_4);
+
+class KEVIN_LCD_18 : public Ml307Board {
+// class KEVIN_LCD_18 : public WifiBoard {
+private:
+    i2c_master_bus_handle_t display_i2c_bus_;
+    Button boot_button_;
+    LcdDisplay* display_;
+    i2c_master_bus_handle_t codec_i2c_bus_;
+    void InitializeCodecI2c() {
+        // Initialize I2C peripheral
+        i2c_master_bus_config_t i2c_bus_cfg = {
+            .i2c_port = I2C_NUM_0,
+            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
+            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
+            .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .intr_priority = 0,
+            .trans_queue_depth = 0,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
+        };
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &codec_i2c_bus_));
+    }
+
+    void Enable4GModule() {
+        // Make GPIO HIGH to enable the 4G module
+        gpio_config_t ml307_enable_config = {
+            .pin_bit_mask = (1ULL << 45),
+            .mode = GPIO_MODE_OUTPUT,
+            .pull_up_en = GPIO_PULLUP_DISABLE,
+            .pull_down_en = GPIO_PULLDOWN_DISABLE,
+            .intr_type = GPIO_INTR_DISABLE,
+        };
+        gpio_config(&ml307_enable_config);
+        gpio_set_level(GPIO_NUM_45, 0);
+    }
+
+    void InitializeSpi() {
+        ESP_LOGI(TAG, "Initialize QSPI bus");
+
+        const spi_bus_config_t bus_config = KEVIN_ST77916_PANEL_BUS_QSPI_CONFIG(QSPI_PIN_NUM_LCD_PCLK,
+                                                                        QSPI_PIN_NUM_LCD_DATA0,
+                                                                        QSPI_PIN_NUM_LCD_DATA1,
+                                                                        QSPI_PIN_NUM_LCD_DATA2,
+                                                                        QSPI_PIN_NUM_LCD_DATA3,
+                                                                        QSPI_LCD_H_RES * 80 * sizeof(uint16_t));
+        ESP_ERROR_CHECK(spi_bus_initialize(QSPI_LCD_HOST, &bus_config, SPI_DMA_CH_AUTO));
+    }
+
+    void Initializest77916Display() {
+
+        esp_lcd_panel_io_handle_t panel_io = nullptr;
+        esp_lcd_panel_handle_t panel = nullptr;
+
+        ESP_LOGI(TAG, "Install panel IO");
+        
+        const esp_lcd_panel_io_spi_config_t io_config = ST77916_PANEL_IO_QSPI_CONFIG(QSPI_PIN_NUM_LCD_CS, NULL, NULL);
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_spi((esp_lcd_spi_bus_handle_t)QSPI_LCD_HOST, &io_config, &panel_io));
+
+        ESP_LOGI(TAG, "Install ST77916 panel driver");
+        
+        st77916_vendor_config_t vendor_config = {
+            .flags = {
+                .use_qspi_interface = 1,
+            },
+        };
+        const esp_lcd_panel_dev_config_t panel_config = {
+            .reset_gpio_num = QSPI_PIN_NUM_LCD_RST,
+            .rgb_ele_order = LCD_RGB_ELEMENT_ORDER_RGB,     // Implemented by LCD command `36h`
+            .bits_per_pixel = QSPI_LCD_BIT_PER_PIXEL,    // Implemented by LCD command `3Ah` (16/18)
+            .vendor_config = &vendor_config,
+        };
+        ESP_ERROR_CHECK(esp_lcd_new_panel_st77916(panel_io, &panel_config, &panel));
+
+        esp_lcd_panel_reset(panel);
+        esp_lcd_panel_init(panel);
+        esp_lcd_panel_disp_on_off(panel, true);
+        esp_lcd_panel_swap_xy(panel, DISPLAY_SWAP_XY);
+        esp_lcd_panel_mirror(panel, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y);
+
+        display_ = new SpiLcdDisplay(panel_io, panel,
+                                    DISPLAY_WIDTH, DISPLAY_HEIGHT, DISPLAY_OFFSET_X, DISPLAY_OFFSET_Y, DISPLAY_MIRROR_X, DISPLAY_MIRROR_Y, DISPLAY_SWAP_XY,
+                                    {
+                                        .text_font = &font_puhui_20_4,
+                                        .icon_font = &font_awesome_20_4,
+                                        .emoji_font = font_emoji_64_init(),
+                                    });
+    }
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting && !WifiStation::GetInstance().IsConnected()) {
+                // ResetWifiConfiguration();
+            }
+        });
+        boot_button_.OnPressDown([this]() {
+            Application::GetInstance().StartListening();
+        });
+        boot_button_.OnPressUp([this]() {
+            Application::GetInstance().StopListening();
+        });
+    }
+
+
+    // 物联网初始化，添加对 AI 可见设备
+    void InitializeIot() {
+        auto& thing_manager = iot::ThingManager::GetInstance();
+        thing_manager.AddThing(iot::CreateThing("Speaker"));
+        thing_manager.AddThing(iot::CreateThing("Screen"));
+    }
+
+public:
+    KEVIN_LCD_18() :  Ml307Board(ML307_TX_PIN, ML307_RX_PIN, 4096),boot_button_(BOOT_BUTTON_GPIO) {
+        ESP_LOGI(TAG, "Initializing KEVIN LCD 1.8 Board");
+        InitializeCodecI2c();
+        InitializeSpi();
+        InitializeButtons();
+        Initializest77916Display();
+        Enable4GModule();
+        InitializeIot();
+        GetBacklight()->RestoreBrightness();
+    }
+    
+
+    // virtual Led* GetLed() override {
+    //     static SingleLed led(BUILTIN_LED_GPIO);
+    //     return &led;
+    // }
+
+    virtual AudioCodec* GetAudioCodec() override {
+        static Es8311AudioCodec audio_codec(codec_i2c_bus_, I2C_NUM_0, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
+        return &audio_codec;
+    }
+
+    virtual Display *GetDisplay() override {
+        return display_;
+    }
+    
+    virtual Backlight* GetBacklight() override {
+        static PwmBacklight backlight(DISPLAY_BACKLIGHT_PIN, DISPLAY_BACKLIGHT_OUTPUT_INVERT);
+        return &backlight;
+    }
+};
+
+DECLARE_BOARD(KEVIN_LCD_18);
